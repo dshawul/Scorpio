656adcb5541c3424fd9e63c0333e9b577ddb18bd
Daniel Abdi <dshawul@yahoo.com>
2018-04-03 16:04:40 -0600

    Retuning eval parameters.

--- params.h
+++ params.h
@@ -1,80 +1,80 @@
 //Automatically generated file. 
 //Any changes made will be lost after tuning. 
-static PARAM PAWN_GUARD = 12;
-static PARAM PAWN_STORM = 6;
-static PARAM KING_ON_OPEN = 21;
-static PARAM PAWN_ATTACK = 16;
-static PARAM KNIGHT_ATTACK = 16;
-static PARAM BISHOP_ATTACK = 16;
-static PARAM ROOK_ATTACK = 32;
-static PARAM QUEEN_ATTACK = 64;
-static PARAM UNDEFENDED_ATTACK = 32;
-static PARAM HANGING_PENALTY = 13;
+static PARAM PAWN_GUARD = 16;
+static PARAM PAWN_STORM = 30;
+static PARAM KING_ON_OPEN = 83;
+static PARAM PAWN_ATTACK = 32;
+static PARAM KNIGHT_ATTACK = 28;
+static PARAM BISHOP_ATTACK = 13;
+static PARAM ROOK_ATTACK = 13;
+static PARAM QUEEN_ATTACK = 44;
+static PARAM UNDEFENDED_ATTACK = 82;
+static PARAM HANGING_PENALTY = 14;
 static PARAM ATTACKED_PIECE = 4;
-static PARAM DEFENDED_PIECE = 5;
-static PARAM KNIGHT_OUTPOST_MG = 40;
+static PARAM DEFENDED_PIECE = 8;
+static PARAM KNIGHT_OUTPOST_MG = 39;
 static PARAM KNIGHT_OUTPOST_EG = 6;
 static PARAM BISHOP_OUTPOST_MG = 34;
-static PARAM BISHOP_OUTPOST_EG = -2;
-static PARAM KNIGHT_MOB_MG = 50;
-static PARAM KNIGHT_MOB_EG = 43;
-static PARAM BISHOP_MOB_MG = 22;
-static PARAM BISHOP_MOB_EG = 15;
+static PARAM BISHOP_OUTPOST_EG = -3;
+static PARAM KNIGHT_MOB_MG = 47;
+static PARAM KNIGHT_MOB_EG = 44;
+static PARAM BISHOP_MOB_MG = 21;
+static PARAM BISHOP_MOB_EG = 14;
 static PARAM ROOK_MOB_MG = 31;
-static PARAM ROOK_MOB_EG = 20;
-static PARAM QUEEN_MOB_MG = -33;
-static PARAM QUEEN_MOB_EG = 189;
-static PARAM BAD_BISHOP_MG = 5;
-static PARAM BAD_BISHOP_EG = 8;
-static PARAM PASSER_BLOCKED = 34;
-static PARAM PASSER_KING_SUPPORT = 9;
-static PARAM PASSER_KING_ATTACK = 41;
-static PARAM PASSER_SUPPORT = 238;
-static PARAM PASSER_ATTACK = 117;
-static PARAM PAWNS_VS_KNIGHT = 30;
-static PARAM CANDIDATE_PP_MG = 23;
-static PARAM CANDIDATE_PP_EG = 58;
+static PARAM ROOK_MOB_EG = 22;
+static PARAM QUEEN_MOB_MG = -7;
+static PARAM QUEEN_MOB_EG = 171;
+static PARAM BAD_BISHOP_MG = 3;
+static PARAM BAD_BISHOP_EG = 9;
+static PARAM PASSER_BLOCKED = 37;
+static PARAM PASSER_KING_SUPPORT = 19;
+static PARAM PASSER_KING_ATTACK = 35;
+static PARAM PASSER_SUPPORT = 241;
+static PARAM PASSER_ATTACK = 141;
+static PARAM PAWNS_VS_KNIGHT = 25;
+static PARAM CANDIDATE_PP_MG = 22;
+static PARAM CANDIDATE_PP_EG = 59;
 static PARAM PAWN_DOUBLED_MG = -1;
-static PARAM PAWN_DOUBLED_EG = 9;
-static PARAM PAWN_ISOLATED_ON_OPEN_MG = 49;
-static PARAM PAWN_ISOLATED_ON_OPEN_EG = 22;
-static PARAM PAWN_ISOLATED_ON_CLOSED_MG = 22;
-static PARAM PAWN_ISOLATED_ON_CLOSED_EG = 13;
-static PARAM PAWN_WEAK_ON_OPEN_MG = 22;
-static PARAM PAWN_WEAK_ON_OPEN_EG = 8;
-static PARAM PAWN_WEAK_ON_CLOSED_MG = 3;
-static PARAM PAWN_WEAK_ON_CLOSED_EG = -1;
-static PARAM PAWN_DUO_MG = 6;
-static PARAM PAWN_DUO_EG = -5;
-static PARAM PAWN_SUPPORTED_MG = 10;
-static PARAM PAWN_SUPPORTED_EG = 5;
-static PARAM ROOK_ON_7TH = 16;
-static PARAM ROOK_ON_OPEN = 15;
-static PARAM ROOK_SUPPORT_PASSED_MG = -28;
-static PARAM ROOK_SUPPORT_PASSED_EG = 55;
-static PARAM TRAPPED_BISHOP = 85;
-static PARAM TRAPPED_KNIGHT = 112;
-static PARAM TRAPPED_ROOK = 97;
-static PARAM EXTRA_PAWN = 130;
-static PARAM QUEEN_MG = 1775;
-static PARAM QUEEN_EG = 1417;
-static PARAM ROOK_MG = 778;
-static PARAM ROOK_EG = 815;
-static PARAM BISHOP_MG = 652;
-static PARAM BISHOP_EG = 492;
-static PARAM KNIGHT_MG = 605;
-static PARAM KNIGHT_EG = 450;
-static PARAM PAWN_MG = 114;
-static PARAM PAWN_EG = 134;
-static PARAM BISHOP_PAIR_MG = 12;
+static PARAM PAWN_DOUBLED_EG = 14;
+static PARAM PAWN_ISOLATED_ON_OPEN_MG = 36;
+static PARAM PAWN_ISOLATED_ON_OPEN_EG = 24;
+static PARAM PAWN_ISOLATED_ON_CLOSED_MG = 13;
+static PARAM PAWN_ISOLATED_ON_CLOSED_EG = 7;
+static PARAM PAWN_WEAK_ON_OPEN_MG = 17;
+static PARAM PAWN_WEAK_ON_OPEN_EG = 12;
+static PARAM PAWN_WEAK_ON_CLOSED_MG = 0;
+static PARAM PAWN_WEAK_ON_CLOSED_EG = 0;
+static PARAM PAWN_DUO_MG = 7;
+static PARAM PAWN_DUO_EG = -4;
+static PARAM PAWN_SUPPORTED_MG = 14;
+static PARAM PAWN_SUPPORTED_EG = 10;
+static PARAM ROOK_ON_7TH = 14;
+static PARAM ROOK_ON_OPEN = 16;
+static PARAM ROOK_SUPPORT_PASSED_MG = -19;
+static PARAM ROOK_SUPPORT_PASSED_EG = 57;
+static PARAM TRAPPED_BISHOP = 89;
+static PARAM TRAPPED_KNIGHT = 104;
+static PARAM TRAPPED_ROOK = 94;
+static PARAM EXTRA_PAWN = 122;
+static PARAM QUEEN_MG = 1734;
+static PARAM QUEEN_EG = 1455;
+static PARAM ROOK_MG = 766;
+static PARAM ROOK_EG = 830;
+static PARAM BISHOP_MG = 639;
+static PARAM BISHOP_EG = 501;
+static PARAM KNIGHT_MG = 599;
+static PARAM KNIGHT_EG = 455;
+static PARAM PAWN_MG = 109;
+static PARAM PAWN_EG = 142;
+static PARAM BISHOP_PAIR_MG = 11;
 static PARAM BISHOP_PAIR_EG = 23;
-static PARAM MAJOR_v_P = 178;
-static PARAM MINOR_v_P = 40;
-static PARAM MINORS3_v_MAJOR = -15;
-static PARAM MINORS2_v_MAJOR = -13;
-static PARAM ROOK_v_MINOR = 37;
-static PARAM TEMPO_BONUS = 31;
-static PARAM TEMPO_SLOPE = 19;
+static PARAM MAJOR_v_P = 156;
+static PARAM MINOR_v_P = 23;
+static PARAM MINORS3_v_MAJOR = -13;
+static PARAM MINORS2_v_MAJOR = -14;
+static PARAM ROOK_v_MINOR = 36;
+static PARAM TEMPO_BONUS = 36;
+static PARAM TEMPO_SLOPE = 13;
 static PARAM king_pcsq[64] = {
   -7,  29, -10,  11, -72, -41, -34, -36,
   -3,   3, -36, -45, -27,  -9,  -3,   1,
@@ -137,49 +137,49 @@ static PARAM pawn_pcsq[64] = {
 };
 static PARAM outpost[32] = {
    0,   0,   0,   0,   0,   0,   0,   0,
-   0,   0,   0,   0,   8,   5,  10,  15,
-  13,  13,   6,  15,   5,  21,   8,  12,
- -22,  -6,   6,   1,   0,   0,   0,   0
+   0,   0,   0,   0,   7,   4,  10,  16,
+  10,  12,   9,  15,   8,  12,   6,  15,
+  -9,   0,  12,   6,   0,   0,   0,   0
 };
 static PARAM passed_rank_bonus[8] = {
-   0,   5,   7,  19,  41,  81, 145,   0
+   0,   3,   5,  17,  36,  78, 143,   0
 };
 static PARAM passed_file_bonus[4] = {
-   6,   5,  -2,  -7
+   7,   5,   1,  -7
 };
 static PARAM qr_on_7thrank[18] = {
- -24,   0,   1, -29, 139,  83, 199, 200,
+ -37,   0,  -3, -39, 135,  56, 198, 200,
  200, 200, 200, 200, 200, 200, 200, 200,
  200, 200
 };
 static PARAM rook_on_hopen[13] = {
- -61, -47, -37, -21,   0,  60,  69,  90,
+-100, -94, -78, -65, -41,  59,  68,  90,
  100, 110, 130, 140, 150
 };
 static PARAM king_to_pawns[8] = {
-   0,   5,  32,  60, 130, 158, 175, 144
+   0,   2,  33,  64, 110, 158, 182, 150
 };
 static PARAM king_on_hopen[8] = {
-   0,  16,  64,  96,  64,  96, 112, 112
+ -11,  24,  70, 111,  59,  93, 119, 127
 };
 static PARAM king_on_file[8] = {
-  32,  16,  48,  64,  64,  32,   0,  16
+  32,  18,  47,  62,  60,  36,  32,  15
 };
 static PARAM king_on_rank[8] = {
-   0,  16,  32,  48,  48,  48,  48,  48
+  38, -11,  35,  58,  55,  48,  48,  48
 };
 static PARAM piece_tropism[8] = {
-   0,  61,  60,  21,  -5,   3,   6, -18
+   0,  44,  34,  20,  -6, -16, -24, -23
 };
 static PARAM queen_tropism[8] = {
-   0,  37, 113,  15, -15, -32,  -7,   1
+   0,  53, 101,  20,  -5, -20,  -8,  22
 };
 static PARAM file_tropism[8] = {
-  85,  94,  67,  53,   9,  -6, -16, -14
+ 135,  73,  66,  48,   6,  -6, -40, -39
 };
 static PARAM ELO_DRAW = 197;
 static PARAM ELO_DRAW_SLOPE_PHASE = -40;
-static PARAM PASSER_WEIGHT_MG = 10;
+static PARAM PASSER_WEIGHT_MG = 11;
 static PARAM PASSER_WEIGHT_EG = 20;
-static PARAM ATTACK_WEIGHT = 21;
-static PARAM TROPISM_WEIGHT = 13;
+static PARAM ATTACK_WEIGHT = 20;
+static PARAM TROPISM_WEIGHT = 16;
