e66fb88fc5bd85c1acaadfa6a604209a1fb45599
Daniel Abdi <dshawul@yahoo.com>
2019-08-16 16:59:36 -0600

    Scorpio 3.0

--- bin/scorpio.bat
+++ bin/scorpio.bat
@@ -6,7 +6,7 @@ cd %~dp0
 
 for /f "delims=" %%a in ('findstr "^egbb_path" scorpio.ini') do @set LN=%%a
 for /f "tokens=* delims= " %%a in ("%LN:~9%") do set LN=%%a
-set PATH=%PATH%;%LN%
+set PATH=%LN%;%PATH%
 
 scorpio.exe %*
 
--- package.sh
+++ package.sh
@@ -2,12 +2,13 @@
 
 set -e
 
-VERSION=290
+VERSION=30
 
 mkdir -p tmp/bin 
 ./build.sh gcc; cp -r bin/ tmp/bin; mv tmp/bin/bin tmp/bin/Linux
 ./build.sh win; cp -r bin/ tmp/bin; mv tmp/bin/bin tmp/bin/Windows;
 mv tmp/bin/Windows/scorpio tmp/bin/Windows/scorpio.exe
+cp uci/*.* tmp/bin/Windows
 ./build.sh arm; cp -r bin/ tmp/bin; mv tmp/bin/bin tmp/bin/Android;
 cd tmp; zip -r ../scorpio$VERSION-mcts-nn.zip bin/; cd ..
 rm -rf tmp
