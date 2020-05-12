7e0d905d35f77deac6a5f355c01e8d4bb8feeb6b
Daniel Shawul <dshawul@yahoo.com>
2020-05-12 16:14:30 -0600

    Added a build docker script.

---
+++ install/build_docker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+VER=3.0
+IMAGE_NAME=scorpio:${VER}
+
+build_image() {
+     docker run --name temp -d -it --gpus=all nvidia/cuda:10.0-runtime-ubuntu18.04 bash
+     docker exec -it temp /bin/bash -c " apt-get -y update && \
+           DEBIAN_FRONTEND=noninteractive apt-get -y install wget unzip && \
+           echo '* hard core 0' >> /etc/security/limits.conf && \
+           echo 'fs.suid_dumpable = 0' >> /etc/sysctl.conf && \
+           useradd scorpio && \
+           mkdir /home/scorpio && \
+           chown scorpio:scorpio /home/scorpio && \
+	   cd /home/scorpio && \
+           wget https://github.com/dshawul/Scorpio/releases/download/${VER}/install.sh && \
+           chmod 755 install.sh && \
+           ./install.sh && \
+	   rm -rf Scorpio/bin/Linux/calibrate.dat"
+     docker commit temp ${1}
+     docker rm -f temp
+}
+
+build_image ${IMAGE_NAME} 
