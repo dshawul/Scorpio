74232f094d60ce123f50b9f2ea07845e21a898ba
Daniel Shawul <dshawul@yahoo.com>
2021-07-29 19:39:30 -0600

    Disable prior AB search, and increase vote weight of NN search.

--- bin/scorpio-mpich.sh
+++ bin/scorpio-mpich.sh
@@ -7,4 +7,4 @@ NDIR=$( grep ^egbb_path scorpio.ini | awk '{ print $2 }' )
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$NDIR
 
 cd $WDIR >/dev/null 2>&1
-exec mpirun -np 1 ./scorpio-mpich "$@" : -np 1 ./scorpio-mpich use_nn 0 montecarlo 0 mt auto/2 "$@"
+exec mpirun -np 1 ./scorpio-mpich vote_weight 200 frac_abprior 0 "$@" : -np 1 ./scorpio-mpich use_nn 0 montecarlo 0 mt auto/2 "$@"
