40b8aa3717ed6af40d84596d1682d49080f8ffa5
Daniel Shawul <dshawul@yahoo.com>
2021-09-07 14:02:00 -0600

    Make cross-compiling mpi binary for windows work

--- package.sh
+++ package.sh
@@ -6,19 +6,25 @@ VERSION=30
 
 mkdir -p tmp/bin 
 cp ScorpioNN.png tmp/bin;
-#linux
+
+#-----linux------
 ./build.sh COMP=clang profile; cp -r bin/ tmp/bin; mv tmp/bin/bin tmp/bin/Linux
 ./build.sh COMP=clang-cluster profile; cp bin/scorpio tmp/bin/Linux/scorpio-mpich
 #copy mpi libs
 cp /usr/lib/mpich/lib/libmpich.so.0 tmp/bin/Linux/libmpich.so.0
 cp /usr/lib/mpich/lib/libmpichcxx.so.0 tmp/bin/Linux/libmpichcxx.so.0
 cp /usr/lib/libcr.so.0 tmp/bin/Linux/libcr.so.0
-#windows
+
+#-----windows------
 ./build.sh COMP=win; cp -r bin/ tmp/bin; mv tmp/bin/bin tmp/bin/Windows;
 mv tmp/bin/Windows/scorpio tmp/bin/Windows/scorpio.exe
-#android
+./build.sh COMP=win-cluster; cp bin/scorpio tmp/bin/Windows/scorpio-mpich.exe
+cp /usr/local/msmpi/Lib/x64/msmpi.dll tmp/bin/Windows/msmpi.dll
+
+#-----android------
 ./build.sh COMP=arm; cp -r bin/ tmp/bin; mv tmp/bin/bin tmp/bin/Android;
-#zip
+#------------------
+
 cd tmp; zip -r ../scorpio$VERSION-mcts-nn.zip bin/; cd ..
 rm -rf tmp
 cd ..
