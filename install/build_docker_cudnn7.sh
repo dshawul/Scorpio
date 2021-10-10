0c266a634607790c0cb42e30482adc9fe7c0fc94
Daniel Shawul <dshawul@yahoo.com>
2021-10-10 11:16:03 -0600

    Add TCEC update script.

--- install/build_docker_cudnn7.sh
+++ install/build_docker_cudnn7.sh
@@ -3,10 +3,17 @@
 VER=3.0
 IMAGE_NAME=scorpio:${VER}
 
+#RAM sizes
+TR=$((45*1024))
+NN=$((16*1024))
+HT=$((32*1024))
+
+BACKS='\\'
+
 build_image() {
      docker run --name temp -d -it --gpus=all ubuntu:18.04 bash
      docker exec -it temp /bin/bash -c " apt-get -y update && \
-           DEBIAN_FRONTEND=noninteractive apt-get -y install wget unzip && \
+           DEBIAN_FRONTEND=noninteractive apt-get -y install wget unzip mpich && \
            echo '* hard core 0' >> /etc/security/limits.conf && \
            echo 'fs.suid_dumpable = 0' >> /etc/sysctl.conf && \
            useradd scorpio && \
@@ -15,8 +22,14 @@ build_image() {
            cd /home/scorpio && \
            wget https://github.com/dshawul/Scorpio/releases/download/${VER}/install.sh && \
            chmod 755 install.sh && \
-           ./install.sh && \
-           sed -i.bak -e 's/^nn_cache_size .*/nn_cache_size 16384/g' -e 's/^nn_cache_size_e .*/nn_cache_size_e 4096/g' Scorpio/bin/Linux/scorpio.ini && \
+           ./install.sh --no-lcnets && \
+           sed -i.bak -e 's/^nn_cache_size .*/nn_cache_size ${NN}/g' \
+                      -e 's/^treeht.*/treeht ${TR}/' \
+                      -e 's/^ht.*/ht ${HT}/' \
+                      -e 's/^egbb_cache_size.*/egbb_cache_size 1024/' \
+                      Scorpio/bin/Linux/scorpio.ini && \
+           sed -i.bak -e 's|.*mpirun.*| exec mpirun -bind-to socket ${BACKS}|' \
+                      Scorpio/bin/Linux/scorpio-mpich.sh && \
            rm -rf Scorpio/bin/Linux/calibrate.dat"
      docker commit temp ${1}
      docker rm -f temp
---
+++ install/update.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+wget https://github.com/dshawul/Scorpio/releases/download/3.0/install.sh
+chmod +x install.sh
+./install.sh --trt 72 --no-egbb --no-lcnets
+
+#RAM sizes
+TR=$((45*1024))
+NN=$((16*1024))
+HT=$((32*1024))
+
+BACKS='\\'
+
+#edit scorpio ini
+sed -i.bak -e "s|^egbb_files_path.*|egbb_files_path /home/scorpio|" \
+           -e "s/^delay.*/delay 0/" \
+           -e "s/^nn_cache_size .*/nn_cache_size ${NN}/" \
+           -e "s/^treeht.*/treeht ${TR}/" \
+           -e "s/^ht.*/ht ${HT}/" \
+           -e "s/^egbb_cache_size.*/egbb_cache_size 1024/" \
+            Scorpio/bin/Linux/scorpio.ini
+
+#edit scorpio-mpich.sh
+sed -i.bak -e 's|.*mpirun.*| exec mpirun -bind-to socket ${BACKS}|' \
+           Scorpio/bin/Linux/scorpio-mpich.sh
+
+EXE=$PWD/Scorpio/bin/Linux/scorpio-mpich.sh
+
+$EXE uci go quit
